54b8e66aaab405bdfce0e3e354a2f6b83e49d85c
hs.lee <hyeonse@hyeonses-MacBook-Pro.local>
2025-10-27 21:35:22 +0900

    Kafka Cluster 구축, Simple Logging

---
+++ common/src/main/java/com/fastcampuspay/common/logging/LoggingProducer.java
@@ -0,0 +1,42 @@
+package com.fastcampuspay.common.logging;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+public class LoggingProducer {
+    private final KafkaProducer<String, String> producer;
+
+    private final String topic;
+
+    public LoggingProducer(
+            @Value("${kafka.clusters.bootstrapservers}") String bootstrapServers,
+            @Value("${logging.topic}") String topic
+    ) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", bootstrapServers);
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        this.producer = new KafkaProducer<>(properties);
+        this.topic = topic;
+    }
+
+    // Kafka Cluster [key, value] Produce
+    public void sendMessage(String key, String value) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+        producer.send(record, (metadata, exception) -> {
+            if (exception == null) {
+                // System.out.println("Message sent successfully. Offset: " + metadata.offset());
+            } else {
+                exception.printStackTrace();
+                // System.err.println("Failed to send message: " + exception.getMessage());
+            }
+        });
+    }
+
+}
